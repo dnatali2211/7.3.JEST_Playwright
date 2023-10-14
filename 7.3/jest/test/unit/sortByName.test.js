f28c6fdec56b7ea1dfbbb0422d67a6bffea53b90
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-10-14 23:54:11 +0400

    Added task 1

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,47 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+    const result = sorting.sortByName(input);
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+    expect(result).toEqual(expected)
   });
-});
+
+  it("Books names should be sorted in descending order", () => {
+    const input = [
+      "Волшебник изумрудного города",
+      "Властелин Колец",
+      "Гарри Поттер",
+    ];
+    const result = sorting.sortByName(input);
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+    expect(result).toEqual(expected)
+  });
+
+  it("Shouldn't change order if names are already sorted", () => {
+    const input = [
+      "Волшебник изумрудного города",
+      "Волшебник изумрудного города",
+      "Волшебник изумрудного города",
+    ];
+    const result = sorting.sortByName(input);
+    const expected = [
+      "Волшебник изумрудного города",
+      "Волшебник изумрудного города",
+      "Волшебник изумрудного города",
+    ];
+    expect(result).toEqual(expected)
+  });
+  })
