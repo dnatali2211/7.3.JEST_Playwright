a76d5168b66b94101da1d3f6a8c3a96bcd1b1480
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-10-18 22:46:41 +0400

    Realised both tasks

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,15 +1,16 @@
 let page;
 
-beforeEach(async () => {
-  page = await browser.newPage();
-  await page.goto("https://github.com/team");
-});
-
-afterEach(() => {
-  page.close();
-});
-
 describe("Github page tests", () => {
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/team");
+  });
+  
+  afterEach(() => {
+    page.close();
+  });
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
@@ -31,4 +32,47 @@ describe("Github page tests", () => {
     const actual = await page.$eval(btnSelector, link => link.textContent);
     expect(actual).toContain("Get started with Team")
   });
+});
+
+describe("Github page title tests", () => {
+
+  afterEach(() => {
+    page.close();
+  });
+
+  test("The h1 title content'", async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/features/actions");
+
+    const link = await page.$("main div div a");
+    await link.click();
+    await page.waitForSelector('main h1');
+
+    const title = "body > div.logged-out.env-production.page-responsive > div.application-main > main > div.p-responsive.container-xl.text-center.mt-12.mb-6 > h1";
+    const actual = await page.$eval(title, link => link.textContent);
+    expect(actual).toContain("The tools you need to build what you want.")
+  }, 10000);
+
+  test("The second link attribute", async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/solutions/ci-cd/");
+
+    const links = await page.$$("a");
+    expect(links.length).toBeGreaterThanOrEqual(2);   
+
+    const secondLink = links[1];
+    const actual = await page.evaluate(link => link.getAttribute('href'), secondLink);
+        expect(actual).toEqual("https://github.com/");
+      });
+
+      test("The h1 title content at the sponsor's page", async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/sponsors");
+
+    const link = await page.$("main div div a");
+    await link.click();
+    await page.waitForSelector('main h1');
+    const title1 = await page.title();
+    expect(title1).toEqual('Explore GitHub Sponsors · GitHub');
+  }, 10000);
 });
